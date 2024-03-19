1ccc337d818c416ad9cb9cc0267e405effd96b12
ammarahmad <ammar.ahmad6410@gmail.com>
2024-03-19 21:39:14 +0400

    add logger plus initial error handler setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
-function user(username: string): string {
-    const name = username;
-    // console.log('name');
-    const a = {
-        n: 'sa',
-    };
-    const c = a.n;
-    return name + c;
-}
+import app from './app';
+import { Config } from './config';
+import logger from './config/logger';
 
-user('Am');
+const startServer = () => {
+    const PORT = Config.PORT;
+
+    try {
+        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1500);
+        }
+    }
+};
+
+startServer();
