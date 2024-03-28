c3c128e4c4f8a9ea731001cfb56a1ada2b26b48d
ammarahmad <ammar.ahmad6410@gmail.com>
2024-03-28 22:28:26 +0400

    add tenants table.

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,8 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '../entity/User';
 import { Config } from '.';
-import { RefreshToken } from '../entity/RefreshToken';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -12,9 +10,9 @@ export const AppDataSource = new DataSource({
     password: Config.DB_PASSWORD,
     database: Config.DB_NAME,
     // Dont use synchronize=true in prod
-    synchronize: true,
+    synchronize: false,
     logging: false,
-    entities: [User, RefreshToken],
-    migrations: [],
+    entities: ['src/entity/*.ts'],
+    migrations: ['src/migration/*.ts'],
     subscribers: [],
 });
