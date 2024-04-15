4c3d93d40918128deffb69f44a47658ed7cc8d88
ammarahmad <ammar.ahmad6410@gmail.com>
2024-04-15 16:56:35 +0400

    add improve error handling.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import 'reflect-metadata';
-import express, { Request, Response, NextFunction } from 'express';
-import logger from './config/logger';
-import { HttpError } from 'http-errors';
+import express, { Request, Response } from 'express';
 import authRouter from './routes/auth';
 import tenantRouter from './routes/tenant';
 import userRouter from './routes/user';
 import cookieParser from 'cookie-parser';
 import cors, { CorsOptions } from 'cors';
 import { Config } from './config';
+import { globalErrorHandler } from './middlewares/globalErrorHandler';
 
 const corsOptions: CorsOptions = {
     origin: [Config.CLIENT_URL!],
@@ -28,20 +27,6 @@ app.use('/auth', authRouter);
 app.use('/tenants', tenantRouter);
 app.use('/users', userRouter);
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-    logger.error(err.message);
-    const statusCode = err.statusCode || err.status || 500;
-    res.status(statusCode).json({
-        errors: [
-            {
-                type: err.name,
-                msg: err.message,
-                path: '',
-                location: '',
-            },
-        ],
-    });
-});
+app.use(globalErrorHandler);
 
 export default app;
